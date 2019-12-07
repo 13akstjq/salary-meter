ca2e5ca2764008c297455264755af66b38cb687e
13akstjq <13akstjq@naver.com>
2019-12-07 23:42:02 +0900

    feat : Add layout land

--- option.js
+++ option.js
@@ -2,6 +2,6 @@ const optionIcon = document.querySelector("#optionIcon");
 const optionContainer = document.querySelector("#optionContainer");
 
 // option toggle func
-const optionShowToggle = () => optionContainer.classList.toggle("option__show");
+const optionShowToggle = () => optionContainer.classList.toggle("option__hide");
 
 optionIcon.addEventListener("click", optionShowToggle);
