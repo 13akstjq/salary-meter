6ea53d97ae53f4e0e2c51595f027b2af1aa7875f
13akstjq <13akstjq@naver.com>
2019-12-08 18:39:05 +0900

    feat : Add chromeStorage

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 // dom
 const date = document.querySelector("#date");
 
-// variables
+// 변수
 let salaryDate,
   preTexSalary,
   postTexSalary,
   monthSalary,
   insurance = [];
 
+// 오늘 날짜를 셋팅하는 메소드
 const settingDate = () => {
   const toDay = new Date();
   const year = toDay.getFullYear();
@@ -16,37 +17,73 @@ const settingDate = () => {
   date.innerHTML = `${year}.${month}.${date}`;
 };
 
-// chrome.storage.sync.set({ value: theValue }, function() {});
-
-const getStorageData = () => {
-  chrome.storage.sync.get("salaryDate", setSalaryDate);
+// 크롬 스토리지에 월급날짜 저장하는 메소드
+const setStorageSalaryDate = salaryDate => {
+  chrome.storage.sync.set({ salaryDate: salaryDate });
 };
 
+// 크롬 스토리지에 세전 연봉 저장하는 메소드
+const setStoragePreTexSalary = preTexSalary => {
+  chrome.storage.sync.set({ preTexSalary: preTexSalary });
+};
+
+// 크롬 스토리지의 값을 불러오는 메소드
+const getStorageData = () => {
+  chrome.storage.sync.get("salaryDate", ({ salaryDate }) => {
+    if (salaryDate) salaryDateInput.value = salaryDate;
+    chrome.storage.sync.get("preTexSalary", ({ preTexSalary }) => {
+      if (preTexSalary) preTexSalaryInput.value = preTexSalary;
+      if (salaryDate && preTexSalary) {
+        console.log(salaryDate, preTexSalary, "start");
+      }
+    });
+  });
+};
+
+// 크롬 스토리지 초기화 메소드
+const clearStorage = () => {
+  chrome.storage.sync.clear();
+};
+// clearStorage();
+// 월급 날짜를 셋팅하는 메소드
 const setSalaryDate = date => {
   salaryDate = date;
 };
 
-const setPreTexSalary = salary => {
+// 세전 연봉을 셋팅하는 메소드
+const setSalary = salary => {
   preTexSalary = salary;
-  calcTex(preTexSalary);
+  postTexSalary = preTexSalary - calcTex(preTexSalary);
+  monthSalary = postTexSalary / 12;
 };
 
+// 세금을 계산하는 메소드
 const calcTex = preTexSalary => {
-  const res = reduce((a, b) => a + b, [1, 2, 3, 4]);
-  console.log(res);
+  let totalTex = 0;
+  for (const a in insurance) {
+    totalTex += preTexSalary * insurance[a];
+  }
+
+  return totalTex;
 };
 
+// 각 세금 퍼센트를 초기화하는 메소드
 const setInsurance = () => {
   insurance["pension"] = 0.045;
   insurance["health"] = 0.0323;
-  insurance["care"] = 0.0851;
-  insurance["salary"] = 0.03;
+  insurance["care"] = 0.00274;
+  insurance["hire"] = 0.008;
+  insurance["salary"] = 0.0225;
+  insurance["regionSalary"] = 0.00225;
 };
 
+// 초기화 메소드
 const init = () => {
   setInsurance();
   settingDate();
-  setPreTexSalary(3400);
+  getStorageData();
+  if (preTexSalary && salaryDate) setSalary(preTexSalary);
 };
 
+// 월급미터기 초기화
 init();
--- js/option.js
+++ js/option.js
@@ -1,7 +1,23 @@
 const optionIcon = document.querySelector("#optionIcon");
 const optionContainer = document.querySelector("#optionContainer");
+const salaryDateInput = document.querySelector("#salaryDateInput");
+const preTexSalaryInput = document.querySelector("#preTexSalaryInput");
 
-// option toggle func
+// 설정 창 토글 메소드
 const optionShowToggle = () => optionContainer.classList.toggle("option__hide");
 
+// 설정 폼 제출 메소드
+const optionSubmitHandler = e => {
+  e.preventDefault();
+  if (salaryDateInput.value && preTexSalaryInput.value) {
+    salaryDate = salaryDateInput.value;
+    preTexSalary = preTexSalaryInput.value;
+    console.log(salaryDate, preTexSalary);
+    setStoragePreTexSalary(preTexSalary);
+    setStorageSalaryDate(salaryDate);
+    optionContainer.classList.toggle("option__hide");
+  }
+};
+
 optionIcon.addEventListener("click", optionShowToggle);
+optionContainer.addEventListener("submit", optionSubmitHandler);
